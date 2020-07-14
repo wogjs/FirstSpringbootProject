da4eb1e418f747e3e5f8df51c21d9bd254017c1f
hy1152 <wogjs1152@gmail.com>
2020-07-14 18:41:02 +0900

    기존 테스트에 시큐리티 적용

--- src/test/java/com/firstProject/springBoot/web/HelloControllerTest.java
+++ src/test/java/com/firstProject/springBoot/web/HelloControllerTest.java
@@ -1,14 +1,17 @@
 package com.firstProject.springBoot.web;
 
+import com.firstProject.springBoot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
-import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -16,13 +19,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)                     //스프링 부트 테스트와 Junit사이에 연결자 역할
-@WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션중 Web에 집중할 수있는 어노테이션
-                                                 // 선언할 경우 @Controller, @ControllerAdvice 등을 사용
-                                                 // @Service, @Component, @Repository 등을 사용x
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
+// 여러 스프링 테스트 어노테이션중 Web에 집중할 수있는 어노테이션
+// 선언할 경우 @Controller, @ControllerAdvice 등을 사용
+// @Service, @Component, @Repository 등을 사용x
 public class HelloControllerTest {
     @Autowired                                   // 스프링이 관리하는 빈을 주입 받는다.
     private MockMvc mvc;                         // 웹 API를 테스트할 때 사용 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles="USER")
     @Test
     public  void hello가_리턴된다() throws  Exception {
         String hello = "hello";
@@ -32,6 +41,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); //mvc.perform의 결과를 검증 응답본문의 내용을 검증 hello를 리턴하기 때문에 이값이 맞는 검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
