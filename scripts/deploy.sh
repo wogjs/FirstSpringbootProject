22457a1dcd2adc44d8104e3ff8c6b5ba62f2663f
hy1152 <wogjs1152@gmail.com>
2020-09-05 16:53:47 +0900

    오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,14 +15,14 @@ CURRENT_PID=$(pgrep -fl myFirstProjecy | grep jar | awk '{print $1}')
 # 스프링부트 애플리케이션 이름(myFirstProjecy)으로 된 다른 프로그램들이 있을 수 있어
 # myFirstProjecy로 된 jar(pgrep -fl myFirstProjecy | grep jar)프로세스를 찾은 뒤 ID를 찾음(awk '{print $1}')
 
-echo " 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
